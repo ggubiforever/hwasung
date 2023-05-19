33860c345ce124873271ccb2fd418381a9a38c30
ggubiforever <jnoo74@gmail.com>
2023-05-19 15:42:11 +0900

    django venv init 2023-05-19 ver4-2

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
